e3654a01bfed92bb58b1b18a34709f2d649e9cb8
Oleksiy Oletsky <oletsky@ukr.net>
2020-09-06 15:44:41 +0300

    Delete simplejsonreader.js

--- simplejsonreader.js
+++
@@ -1,8 +0,0 @@
-var fs=require('fs');
-var info=fs.readFileSync('info.json');
-var coll=JSON.parse(info);
-for (let obj in coll) {
-    console.log(coll[obj].name);
-    console.log(coll[obj].age)
-    console.log("*********");
-}
