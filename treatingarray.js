f6d16d29b08664b44f3622af6f521a882826d953
Oleksiy Oletsky <oletsky@ukr.net>
2020-09-06 15:58:05 +0300

    Add files via upload

---
+++ treatingarray.js
@@ -0,0 +1,9 @@
+"using strict";
+let fs=require('fs');
+let info=fs.readFileSync('manyobjects.json');
+let coll=JSON.parse(info);
+for (let obj in coll) {
+    console.log(coll[obj].name);
+    console.log(coll[obj].age)
+    console.log("*********");
+}
