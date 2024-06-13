665c7574c63f31abf2e172a08c5fd910f022ea09
Oleksiy Oletsky <oletsky@ukr.net>
2024-06-13 14:33:32 +0300

    New changes

--- treatingarray.js
+++ treatingarray.js
@@ -5,5 +5,5 @@ let coll=JSON.parse(info);
 for (let obj in coll) {
     console.log(coll[obj].name);
     console.log(coll[obj].age)
-    console.log("***********That's al********************");
+    console.log("*******************************");
 }
